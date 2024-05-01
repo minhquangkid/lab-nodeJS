8547fd59b5ce91353cadebdb8280679dc9c88ab9
quang <minhquangkid@gmail.com>
2024-05-01 18:19:52 +0700

    xong lab-5.1

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-
-const mainRoutes = require('./routes/index');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "pug"); // dùng pub cho template html
+app.set("views", "views"); // chọn folder views làm view cho template html
+//https://expressjs.com/en/api.html#app.set
+//https://expressjs.com/en/guide/using-template-engines.html
 
-app.use(mainRoutes);
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
 
 app.listen(3000);
