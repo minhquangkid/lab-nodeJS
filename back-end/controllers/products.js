79576d7db66ea60148daf4f99c6e364abb034a9a
quang <minhquangkid@gmail.com>
2024-05-08 21:59:17 +0700

    xong ;ab 7.1

--- back-end/controllers/products.js
+++ back-end/controllers/products.js
@@ -3,11 +3,20 @@ const Product = require("../models/product");
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((data) => {
     console.log(data);
+    res.send(data);
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   console.log(req.body);
+  const product = new Product(
+    req.body.title,
+    req.body.imageUrl,
+    req.body.description,
+    req.body.price
+  );
+  product.save();
+  res.status(200).send(true);
   // const product = new Product(req.body.title);
   // product.save();
   // res.redirect('/');
--- front-end/src/components/add-product.jsx
+++ front-end/src/components/add-product.jsx
@@ -1,45 +1,64 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "./navigate";
 import "../CSS/forms.css";
+import { useNavigate } from "react-router-dom";
 const AddProduct = (props) => {
-  const [list, setList] = useState([]);
+  const navigate = useNavigate();
 
-  // const sendData = () => {
-  //   console.log(user);
-  //   fetch("http://localhost:5000/add-user", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //     body: JSON.stringify({ user: user }),
-  //   })
-  //     .then((r) => {
-  //       console.log(r);
-  //       return r.json();
-  //     })
-  //     .then((result) => {
-  //       console.log(result);
-  //     });
-  // };
+  const titleRef = useRef(null);
+  const imageUrlRef = useRef(null);
+  const priceRef = useRef(null);
+  const descriptionRef = useRef(null);
 
-  // const changeHandle = (e) => {
-  //   setUser(e.target.value);
-  // };
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Access input values using refs
+    const title = titleRef.current.value;
+    const imageUrl = imageUrlRef.current.value;
+    const price = priceRef.current.value;
+    const description = descriptionRef.current.value;
+
+    const formData = {
+      title,
+      imageUrl,
+      price,
+      description,
+    };
+
+    // Send the form data using fetch
+    try {
+      const response = await fetch("http://localhost:5000/admin/add-product", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        navigate("/");
+      } else {
+        console.error("Failed to add product:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error adding product:", error);
+    }
+  };
 
   return (
     <Fragment>
       <Navigation></Navigation>
-      <form className="product-form" action="/admin/add-product" method="POST">
+      <form className="product-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
+          <input type="text" name="title" id="title" ref={titleRef} />
 
           <label htmlFor="imageUrl">Image Url</label>
-          <input type="text" name="imageUrl" id="imageUrl" />
+          <input type="text" name="imageUrl" id="imageUrl" ref={imageUrlRef} />
 
           <label htmlFor="price">Price</label>
-          <input type="text" name="price" id="price" />
+          <input type="text" name="price" id="price" ref={priceRef} />
 
           <label htmlFor="description">Description</label>
           <textarea
@@ -48,6 +67,7 @@ const AddProduct = (props) => {
             id="description"
             cols="10"
             rows="5"
+            ref={descriptionRef}
           />
         </div>
 
